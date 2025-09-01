6562437aed33944735810bff83b1893cd0ecc29c
Melik1986 <melikmusinian@gmail.com>
2025-09-01 11:15:40 +0200

    Refactor code structure for improved readability and maintainability

--- project_threejs_ai-3D/client/src/canvas/index.jsx
+++ project_threejs_ai-3D/client/src/canvas/index.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
+import { Suspense } from 'react';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
@@ -13,8 +14,7 @@ const CanvasModel = () => {
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
     >
-      <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+  <ambientLight intensity={0.5} />
 
       <CameraRig>
         <Backdrop />
